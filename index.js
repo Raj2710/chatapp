693e616ad119890bcd119a327332a2ff3b0f9a8c
Raj2710 <nagarajansai2727@gmail.com>
2021-08-02 10:45:13 +0530

    Revoked Changes

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ const PORT = process.env.PORT || 5000
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
-const corsOptions={
-    cors: true,
-    origins:["https://funroom.netlify.app"],
-   }
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server,corsOptions);
+const io = socketio(server);
 
 app.use(cors());
 app.use(router);
